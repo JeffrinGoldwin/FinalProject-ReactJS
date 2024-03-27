5f973318f62d2e76de0082a9d3afdd43d67a6fdf
Jeffrin Goldwin R C <jeffringoldwin@jmangroup.com>
2024-03-27 17:17:15 +0530

    useEffect Added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { LoginPage } from './components/LoginPage';
+import axios from 'axios'
 
 function App() {
+  const [users, setusers] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3001/getUsers')
+    .then(users => setusers(users.data))
+    .catch(err => console.log(err))
+    console.log(users)
+  }, [])
+
+
   return (
     <div className="App">
-      <LoginPage />
+      Name : 
+      {
+        users.map(users => {
+           return (<p>{users.FirstName}</p>)
+        })
+      }
     </div>
   );
 }
