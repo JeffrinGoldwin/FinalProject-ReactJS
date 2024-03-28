1637d5b7eb3dda8b8ca75e10605868722b65a286
Jeffrin Goldwin R C <jeffringoldwin@jmangroup.com>
2024-03-28 12:25:50 +0530

    Did Routing

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,45 +1,60 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { LoginPage } from './components/LoginPage';
-import { LandingPage } from './components/LandingPage';
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { LoginPage } from "./components/LoginPage";
+import { LandingPage } from "./components/LandingPage";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 
 function App() {
-  const [users, setusers] = useState([])
-  const [loginEmail, setloginEmail] = useState("Admin@gmail.com")
-  const [loginPassword, setloginPassword] = useState("Admin")
+  const navigate = useNavigate();
+  const [users, setusers] = useState([]);
+  const [loginEmail, setloginEmail] = useState("Admin@gmail.com");
+  const [loginPassword, setloginPassword] = useState("Admin");
 
   useEffect(() => {
-    axios.get('http://localhost:3001/getUsers')
-    .then(users => setusers(users.data))
-    .catch(err => console.log(err))
-    console.log(users)
-  }, [])
+    axios
+      .get("http://localhost:3001/getUsers")
+      .then((response) => setusers(response.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    users.forEach(obj => {
-      const checker = obj.Email = loginEmail && obj.Password == loginPassword
+    users.forEach((obj) => {
+      console.log(obj.Email)
+      const checker = obj.Email === loginEmail && obj.Password === loginPassword;
       if (checker) {
-        console.log('Login successful');
-        return ;
+        console.log("Login successful");
+        navigate('/LandingPage');
+        ;
       } else {
-        console.log('Invalid email or password');
+        console.log("Invalid email or password");
       }
-    })
-  }
+    });
+  };
 
   return (
     <div className="App">
-        {/* <LoginPage 
+      {/* <LoginPage 
           loginEmail={loginEmail}
           setloginEmail={setloginEmail}
           loginPassword={loginPassword}
           setloginPassword={setloginPassword}
           handleLogin={handleLogin}
         /> */}
-        <LandingPage />
+      {/* <LandingPage /> */}
+      
+        <Routes>
+          <Route path="/login" element={<LoginPage loginEmail={loginEmail}
+          setloginEmail={setloginEmail}
+          loginPassword={loginPassword}
+          setloginPassword={setloginPassword}
+          handleLogin={handleLogin}
+          />} />
+          <Route path="/LandingPage" element={<LandingPage />} />
+        </Routes>
     </div>
   );
 }
