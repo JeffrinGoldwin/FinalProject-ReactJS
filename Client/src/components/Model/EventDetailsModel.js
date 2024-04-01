d3e3ce1bba6aed657b4e34d51162fc8ba7cbc474
Jeffrin Goldwin R C <jeffringoldwin@jmangroup.com>
2024-04-01 17:08:11 +0530

    Littles changes to AcceptReject Mechanism

--- Client/src/components/Model/EventDetailsModel.js
+++ Client/src/components/Model/EventDetailsModel.js
@@ -1,88 +1,168 @@
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import axios from 'axios';
-
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import axios from "axios";
 
 export const EventDetailsModel = ({
   eventDetailsModelshow,
   handleClose,
   event,
 }) => {
+  const [currentUser, setCurrentUser] = useState([]);
+  const [email, setemail] = useState("");
+  const [eventTitle, seteventTitle] = useState("");
 
-    const [currentUser, setCurrentUser] = useState([]);
-    const [showDropdown, setShowDropdown] = useState(false);
-    const [email, setemail] = useState('');
-    const [eventTitle, seteventTitle] = useState('');
-
-    useEffect(() => {
-        const fetchCurrentUser = async () => {
-          try {
-            const response = await axios.get("http://localhost:3001/currentUser");
-            setCurrentUser(response.data);
-          } catch (error) {
-            console.error("Error fetching current user:", error);
-            setCurrentUser(null); // Reset currentUser if there's an error
-          }
-        };
-    
-        fetchCurrentUser();
-      }, []);
-
-      useEffect(() => {
-        const checkAcceptReject = async () => {
-          try {
-            const response = await axios.get(`http://localhost:3001/checkAcceptReject?email=${currentUser.Email}&eventName=${event.EventName}`);
-            // Check if the response contains data
-            if (response.data) {
-              // Set the email and event title from the response data
-              setemail(response.data.Email);
-              seteventTitle(response.data.EventName);
-              console.log(response.data.Email)
-              console.log(response.data.EventName)
-              console.log(currentUser.Email)
-              console.log(event.EventName)
-              // If the response data exists, set showDropdown to true
-              setShowDropdown(true);
-            }
-          } catch (error) {
-            console.error('Error checking accept/reject:', error);
-          }
-        };
-      
-        checkAcceptReject();
-      }, [currentUser.Email, event.EventName]);
-
-    const handleAccept = async () => {
-        try {
-            // Increment event.accept by 1 locally
-            const updatedEvent = { ...event, Accepted: event.Accepted + 1 };
-            console.log("up data", updatedEvent)
-        
-            // Make an API call to update the event in the database
-            const response = await axios.put(`http://localhost:3001/updateEvent/${event._id}`, updatedEvent);
-            
-            // Check if the update was successful
-            if (response.status === 200) {
-              console.log('Event accept incremented successfully:', response.data);
-              handleClose(); // Close the modal
-              const acceptRejectData = {
-                Email: currentUser.Email, 
-                EventName: event.EventName,
-                AcceptOrReject: '1'
-                };
-                const acceptRejectResponse = await axios.post('http://localhost:3001/addAcceptReject', acceptRejectData);
-                console.log('Accept/reject data added successfully:', acceptRejectResponse.data);
-            } else {
-              console.error('Failed to increment event accept:', response.statusText);
-            }
-          } catch (error) {
-            console.error('Error incrementing event accept:', error);
-          }
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/currentUser");
+        setCurrentUser(response.data);
+      } catch (error) {
+        console.error("Error fetching current user:", error);
+        setCurrentUser(null); // Reset currentUser if there's an error
+      }
     };
+
+    fetchCurrentUser();
+  }, []);
+
+  useEffect(() => {
+    const checkAcceptReject = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/checkAcceptReject?email=${currentUser.Email}&eventName=${event.EventName}`
+        );
+        // Check if the response contains data
+        if (response.data) {
+          // Set the email and event title from the response data
+          setemail(response.data.Email);
+          seteventTitle(response.data.EventName);
+          console.log(response.data.Email);
+          console.log(response.data.EventName);
+          console.log(currentUser.Email);
+          console.log(event.EventName);
+        }
+      } catch (error) {
+        console.error("Error checking accept/reject:", error);
+      }
+    };
+
+    checkAcceptReject();
+  }, [currentUser.Email, event.EventName]);
+
+  const handleAccept = async () => {
+    try {
+      // Increment event.accept by 1 locally
+      const updatedEvent = { ...event, Accepted: event.Accepted + 1 };
+      console.log("up data", updatedEvent);
+
+      // Make an API call to update the event in the database
+      const response = await axios.put(
+        `http://localhost:3001/updateEvent/${event._id}`,
+        updatedEvent
+      );
+
+      // Check if the update was successful
+      if (response.status === 200) {
+        console.log("Event accept incremented successfully:", response.data);
+        handleClose(); // Close the modal
+        const acceptRejectData = {
+          Email: currentUser.Email,
+          EventName: event.EventName,
+          AcceptOrReject: "1",
+        };
+        const acceptRejectResponse = await axios.post(
+          "http://localhost:3001/addAcceptReject",
+          acceptRejectData
+        );
+        console.log(
+          "Accept/reject data added successfully:",
+          acceptRejectResponse.data
+        );
+      } else {
+        console.error("Failed to increment event accept:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error incrementing event accept:", error);
+    }
+  };
+
+  const handleReject = async () => {
+    try {
+      const updatedEvent = { ...event, Rejected: event.Rejected + 1 };
+      console.log("up data", updatedEvent);
+
+      // Make an API call to update the event in the database
+      const response = await axios.put(
+        `http://localhost:3001/updateEvent/${event._id}`,
+        updatedEvent
+      );
+
+      // Check if the update was successful
+      if (response.status === 200) {
+        console.log("Event reject incremented successfully:", response.data);
+        handleClose(); // Close the modal
+        const acceptRejectData = {
+          Email: currentUser.Email,
+          EventName: event.EventName,
+          AcceptOrReject: "2",
+        };
+        const acceptRejectResponse = await axios.post(
+          "http://localhost:3001/addAcceptReject",
+          acceptRejectData
+        );
+        console.log(
+          "Accept/reject data added successfully:",
+          acceptRejectResponse.data
+        );
+      } else {
+        console.error("Failed to increment event accept:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error incrementing event accept:", error);
+    }
+  };
+
+  const handleMaybe = async () => {
+    try {
+      const updatedEvent = { ...event, Maybe: event.Maybe + 1 };
+      console.log("up data", updatedEvent);
+
+      // Make an API call to update the event in the database
+      const response = await axios.put(
+        `http://localhost:3001/updateEvent/${event._id}`,
+        updatedEvent
+      );
+
+      // Check if the update was successful
+      if (response.status === 200) {
+        console.log("Event reject incremented successfully:", response.data);
+        handleClose(); // Close the modal
+        const acceptRejectData = {
+          Email: currentUser.Email,
+          EventName: event.EventName,
+          AcceptOrReject: "3",
+        };
+        const acceptRejectResponse = await axios.post(
+          "http://localhost:3001/addAcceptReject",
+          acceptRejectData
+        );
+        console.log(
+          "Accept/reject data added successfully:",
+          acceptRejectResponse.data
+        );
+      } else {
+        console.error("Failed to increment event maybe:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error incrementing event maybe:", error);
+    }
+  };
+
+
 
   return (
     <div>
@@ -119,16 +199,28 @@ export const EventDetailsModel = ({
           </p>
         </Modal.Body>
         <Modal.Footer>
-        {!(currentUser.Email === email && eventTitle === event.EventName) && (
+        {currentUser.Role === "User" && (
+            <div>
+          {(currentUser.Email === email && eventTitle === event.EventName) ? (
+            <Button variant="secondary" disabled>Answered</Button>
+          ) : (
             <Dropdown as={ButtonGroup}>
-                <Button variant="primary" onClick={handleAccept}>Accept</Button>
-                <Dropdown.Toggle split variant="primary" id="dropdown-split-basic" />
-                <Dropdown.Menu>
-                    <Dropdown.Item>{event.Accepted}</Dropdown.Item>
-                    <Dropdown.Item>Maybe</Dropdown.Item>
-                </Dropdown.Menu>
+              <Button variant="primary" onClick={handleAccept}>
+                Accept
+              </Button>
+              <Dropdown.Toggle
+                split
+                variant="primary"
+                id="dropdown-split-basic"
+              />
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={handleReject}>Reject</Dropdown.Item>
+                <Dropdown.Item onClick={handleMaybe}>Maybe</Dropdown.Item>
+              </Dropdown.Menu>
             </Dropdown>
-        )}
+          )}
+          </div>
+          )}
         </Modal.Footer>
       </Modal>
     </div>
